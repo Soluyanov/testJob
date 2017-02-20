fa0f8a6ff08ac82df5348aefbd2e50d6fc13919e
Soluyanov <siluyanov.a.v@gmail.com>
2017-02-20 11:46:30 +0300

    add readme file

--- src/main/scala/com/test/core/streaming/Fake.scala
+++ src/main/scala/com/test/core/streaming/Fake.scala
@@ -11,18 +11,19 @@ object Fake {
 
 val Log = Logger.getLogger(Fake.this.getClass().getSimpleName())
   def main(args: Array[String]) {
-/**    
+    
 if (args.length < 3) {
       System.err.println(
-        "Usage: KafkaWordCountProducer <metadataBrokerList> <topic> " +
-          "<messagesPerSec> <wordsPerMessage>")
+        "Usage: test message " +
+          "path/to/spam path/to/ham")
       System.exit(1)
     }
-*/
+
+  val Array(testMessage, pathToSpamSet, pathToHamSet) = args
   val conf = new SparkConf().setAppName("Fake").setMaster("local[*]")
   val sc = new SparkContext(conf)
-  val spam = sc.textFile("/home/alexander/Downloads/spam1.txt", 4)
-  val normal = sc.textFile("/home/alexander/Downloads/ham1.txt", 4)
+  val spam = sc.textFile(pathToSpamSet)
+  val normal = sc.textFile(pathToHamSet)
   val tf = new HashingTF(numFeatures = 10000)
   val spamFeatures = spam.map(email => tf.transform(email.split(" ")))
   val normalFeatures = normal.map(email => tf.transform(email.split(" ")))
@@ -31,12 +32,9 @@ if (args.length < 3) {
   val trainingData = positiveExamples.union(negativeExamples)
   trainingData.cache()
   val model = new LogisticRegressionWithSGD().run(trainingData)
-  //Test on a positive example (spam) and a negative one (normal).
-  val posTest = tf.transform(
-    "REMINDER FROM O2  To get 2 50 pounds free call credit and details of great offers pls reply 2 this text with your valid name  house no and postcode".split(" "))
-  val negTest = tf.transform(
-    "Ok which your another number".split(" "))
-  println("Prediction for positive test example: " + model.predict(posTest))
-  println("Prediction for negative test example: " + model.predict(negTest))
+  
+  val test = tf.transform(testMessage.split(" "))
+  println("Prediction for test example: " + model.predict(test))
+  
 }
 }
